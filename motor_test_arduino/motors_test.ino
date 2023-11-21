3fd5c710c19252636402051ec1830c05e65f91eb
creepermystery <52111693+creepermystery@users.noreply.github.com>
2023-11-21 16:08:22 +0100

    Adaptation du code pour nos besoins

--- motor_test_arduino/motors_test.ino
+++ motor_test_arduino/motors_test.ino
@@ -19,7 +19,7 @@ void pwmSet(uint8_t concerned_pin, uint32_t value) {
   analogWrite(concerned_pin, value);
 }
 
-void Motor1_control(int sp) {
+void Motor_control(int sp) {
   if (sp < 0) {
     digitalWrite(DIR1, LOW);
     sp = -sp;
@@ -29,45 +29,17 @@ void Motor1_control(int sp) {
   pwmSet(PWM1, sp > 255 ? 255 : 255 - sp);
 }
 
-void Motor2_control(int sp) {
-  if (sp < 0) {
-    digitalWrite(DIR2, LOW);
-    sp = -sp;
-  } else {
-    digitalWrite(DIR2, HIGH);
-  }
-  pwmSet(PWM2, sp > 255 ? 255 : 255 - sp);
-}
-
-void Motor3_control(int sp) {
-  if (sp < 0) {
-    digitalWrite(DIR3, LOW);
-    sp = -sp;
-  } else {
-    digitalWrite(DIR3, HIGH);
-  }
-  pwmSet(PWM3, sp > 255 ? 255 : 255 - sp);
-}
-
 void setup() {
-
   pinMode(BRAKE, OUTPUT);
   digitalWrite(BRAKE, HIGH);
   
   pinMode(DIR1, OUTPUT);
   Motor1_control(0);
   
-  pinMode(DIR2, OUTPUT);
-  Motor2_control(0);
-  
-  pinMode(DIR3, OUTPUT);
-  Motor3_control(0);
-
   delay(2000);
 }
 
 void loop() {
-
     Motor1_control(10);
     delay(2000);
     Motor1_control(30);
@@ -79,31 +51,5 @@ void loop() {
     Motor1_control(-30);
     delay(2000);
     Motor1_control(0);
-    delay(2000);
-
-    Motor2_control(10);
-    delay(2000);
-    Motor2_control(30);
-    delay(2000);
-    Motor2_control(0);
-    delay(2000);
-    Motor2_control(-10);
-    delay(2000);
-    Motor2_control(-30);
-    delay(2000);
-    Motor2_control(0);
-    delay(2000);
-
-    Motor3_control(10);
-    delay(2000);
-    Motor3_control(30);
-    delay(2000);
-    Motor3_control(0);
-    delay(2000);
-    Motor3_control(-10);
-    delay(2000);
-    Motor3_control(-30);
-    delay(2000);
-    Motor3_control(0);
     delay(2000);
 }
