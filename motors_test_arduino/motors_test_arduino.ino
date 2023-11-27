e94c16168b0cbf28b6cd7d441063fb811d8360ca
jcaignet <julie.caignet@student.junia.com>
2023-11-27 11:59:28 +0100

    le moteur il tourne là

--- motors_test_arduino/motors_test_arduino.ino
+++ motors_test_arduino/motors_test_arduino.ino
@@ -3,10 +3,6 @@
 #define PWM         3
 #define START       8
 
-void pwmSet(uint8_t concerned_pin, uint32_t value) {
-  analogWrite(concerned_pin, value);
-}
-
 void Motor_control(int sp) {
   if (sp < 0) {
     digitalWrite(DIR, LOW);
@@ -14,32 +10,28 @@ void Motor_control(int sp) {
   } else {
     digitalWrite(DIR, HIGH);
   }
-  pwmSet(PWM, sp > 255 ? 255 : 255 - sp);
+  analogWrite(PWM, sp > 255 ? 255 : sp);
 }
 
 void setup() {
   pinMode(BRAKE, OUTPUT);
   pinMode(PWM, OUTPUT);
   pinMode(START, OUTPUT);
+  pinMode(DIR, OUTPUT);
   digitalWrite(START, HIGH);
   digitalWrite(BRAKE, HIGH);
-  
-  pinMode(DIR, OUTPUT);
-  analogWrite(3, 0);
   delay(2000);
 }
 
 void loop() {
-    Motor_control(10);
-    delay(2000);
-    Motor_control(30);
-    delay(2000);
+    Motor_control(200);
+    delay(3000);
     Motor_control(0);
-    delay(2000);
-    Motor_control(-10);
-    delay(2000);
-    Motor_control(-30);
-    delay(2000);
+    delay(3000);
+    Motor_control(-100);
+    delay(3000);
     Motor_control(0);
-    delay(2000);
+    delay(3000);
+    Motor_control(20);
+    delay(3000);
 }
