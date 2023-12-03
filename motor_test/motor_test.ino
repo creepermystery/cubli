afb3bb452e8ee4b60464c5c394ac878475941cf6
creepermystery <mccreepermystery>
2023-12-03 19:38:05 +0100

    Testing weird motor function quirks

--- motor_test/motor_test.ino
+++ motor_test/motor_test.ino
@@ -1,37 +1,19 @@
-#define BRAKE       2
-#define DIR         4
-#define PWM         3
+#define BRAKE       7
+#define DIR        4
+#define PWMR        3
 #define START       8
 
-void Motor_control(int sp) {
-  if (sp < 0) {
-    digitalWrite(DIR, LOW);
-    sp = -sp;
-  } else {
-    digitalWrite(DIR, HIGH);
-  }
-  analogWrite(PWM, sp > 255 ? 255 : sp);
-}
-
 void setup() {
+  Serial.begin(9600);
   pinMode(BRAKE, OUTPUT);
-  pinMode(PWM, OUTPUT);
+  pinMode(PWMR, OUTPUT);
   pinMode(START, OUTPUT);
   pinMode(DIR, OUTPUT);
-  digitalWrite(START, HIGH);
-  digitalWrite(BRAKE, HIGH);
+
+  
+  analogWrite(PWMR, 20);
   delay(2000);
 }
 
 void loop() {
-    Motor_control(200);
-    delay(3000);
-    Motor_control(0);
-    delay(3000);
-    Motor_control(-100);
-    delay(3000);
-    Motor_control(0);
-    delay(3000);
-    Motor_control(20);
-    delay(3000);
 }
